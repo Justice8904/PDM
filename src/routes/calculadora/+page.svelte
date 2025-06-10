85e7672aba829bdb5ce5678b81bacd46c7d45202
justice8904 <joao.cavassini12@gmail.com>
2025-06-10 08:24:46 -0400

    Celebração beta do Dimensions (Atualização da Calculadora)

--- src/routes/calculadora/+page.svelte
+++ src/routes/calculadora/+page.svelte
@@ -13,11 +13,16 @@
 
     function equals(){
         display = eval(display)
+        display = String(display)
     }
 
     function clear(){
         display=""
     }
+    function invert(){
+        display *= -1
+    }
+
 </script>
 <div class="text-center">
     <input class="form-control-lg w-100 mt-3" readonly  bind:value={display}/>
@@ -29,30 +34,36 @@
                 <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("(")}>&lpar;</button></td>
                 <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press(")")}>&rpar;</button></td>
                 <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("/")}>/</button></td>
+                <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("**")}>x<sup>y</sup></button></td>
             </tr>
             <tr>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(7)}>7</button></td>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(8)}>8</button></td>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(9)}>9</button></td>
                 <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("*")}>X</button></td>
+                <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("Math.sqrt(")}>&radic;</button></td>
+                <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("**1/")}><sup>x</sup>&radic;</button></td>
             </tr>
             <tr>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(4)}>4</button></td>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(5)}>5</button></td>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(6)}>6</button></td>
                 <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("-")}>&minus;</button></td>
+                <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("Math.round(")}>Round</button></td>
+                <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => invert()}>+/-</button></td>
             </tr>
             <tr>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(1)}>1</button></td>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(2)}>2</button></td>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(3)}>3</button></td>
                 <td rowspan="2" style="height: 0;"><button type="button" class="btn btn-outline-warning w-100 h-100" onclick={() => press("+")}>&plus;</button></td>
+                <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("Math.log(")}>Log</button></td>
             </tr>
             <tr>
                 <td><button type="button" class="btn btn-outline-dark w-100" onclick={() => press(0)}>0</button></td>
                 <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press(".")}>&sdot;</button></td>
                 <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => equals()}>&equals;</button></td>
-                <td></td>
+                <td><button type="button" class="btn btn-outline-warning w-100" onclick={() => press("%")}>&percnt;</button></td>
             </tr>
         </tbody>
     </table>
